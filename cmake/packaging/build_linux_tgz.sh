77b27e02386710ea48e2e775ae393bc205228bfe
Axel Kohlmeyer <akohlmey@gmail.com>
2025-08-27 22:46:56 -0400

    generalize script to build linux .tgz file to support Qt5 *and* Qt6

--- cmake/packaging/build_linux_tgz.sh
+++ cmake/packaging/build_linux_tgz.sh
@@ -5,7 +5,7 @@ DESTDIR=${PWD}/../LAMMPS_GUI
 VERSION="$1"
 
 echo "Delete old files, if they exist"
-rm -rf ${DESTDIR} ../LAMMPS_GUI-Linux-amd64*.tar.gz
+rm -rf ${DESTDIR} ../LAMMPS-GUI-Linux-amd64*.tar.gz
 
 echo "Create staging area for deployment and populate"
 DESTDIR=${DESTDIR} cmake --install .  --prefix "/"
@@ -25,18 +25,18 @@ rm -f ${DESTDIR}/lib/libgcc_s*
 rm -f ${DESTDIR}/lib/libstdc++*
 
 # get qt dir
-QTDIR=$(ldd ${DESTDIR}/bin/lammps-gui | grep libQt5Core | sed -e 's/^.*=> *//' -e 's/libQt5Core.so.*$/qt5/')
+QTDIR=$(ldd ${DESTDIR}/bin/lammps-gui | grep libQt.Core | sed -e 's/^.*=> *//' -e 's/libQt\(.\)Core.so.*$/qt\1/')
 cat > ${DESTDIR}/bin/qt.conf <<EOF
 [Paths]
-Plugins = ../qt5plugins
+Plugins = ../qtplugins
 EOF
 
 # platform plugin
-mkdir -p ${DESTDIR}/qt5plugins/platforms
-cp ${QTDIR}/plugins/platforms/libqxcb.so ${DESTDIR}/qt5plugins/platforms
+mkdir -p ${DESTDIR}/qtplugins/platforms
+cp ${QTDIR}/plugins/platforms/libqxcb.so ${DESTDIR}/qtplugins/platforms
 
 # get platform plugin dependencies
-QTDEPS=$(LD_LIBRARY_PATH=${DESTDIR}/lib ldd ${QTDIR}/plugins/platforms/libqxcb.so | grep -v ${DESTDIR} | grep libQt5 | sed -e 's/^.*=> *//' -e 's/\(libQt5.*.so.*\) .*$/\1/')
+QTDEPS=$(LD_LIBRARY_PATH=${DESTDIR}/lib ldd ${QTDIR}/plugins/platforms/libqxcb.so | grep -v ${DESTDIR} | grep libQt[56] | sed -e 's/^.*=> *//' -e 's/\(libQt[56].*.so.*\) .*$/\1/')
 for dep in ${QTDEPS}
 do \
     cp ${dep} ${DESTDIR}/lib
@@ -45,13 +45,13 @@ done
 echo "Add additional plugins for Qt"
 for dir in styles imageformats
 do \
-    cp -r  ${QTDIR}/plugins/${dir} ${DESTDIR}/qt5plugins/
+    cp -r  ${QTDIR}/plugins/${dir} ${DESTDIR}/qtplugins/
 done
 
 # get imageplugin dependencies
-for s in ${DESTDIR}/qt5plugins/imageformats/*.so
+for s in ${DESTDIR}/qtplugins/imageformats/*.so
 do \
-    QTDEPS=$(LD_LIBRARY_PATH=${DESTDIR}/lib ldd $s | grep -v ${DESTDIR} | grep -E '(libQt5|jpeg)' | sed -e 's/^.*=> *//' -e 's/\(lib.*.so.*\) .*$/\1/')
+    QTDEPS=$(LD_LIBRARY_PATH=${DESTDIR}/lib ldd $s | grep -v ${DESTDIR} | grep -E '(libQt.|jpeg)' | sed -e 's/^.*=> *//' -e 's/\(lib.*.so.*\) .*$/\1/')
     for dep in ${QTDEPS}
     do \
         cp ${dep} ${DESTDIR}/lib
@@ -72,7 +72,7 @@ do \
 done
 
 pushd ..
-tar -czvvf LAMMPS_GUI-Linux-amd64-${VERSION}.tar.gz LAMMPS_GUI
+tar -czvvf LAMMPS-GUI-Linux-amd64-${VERSION}.tar.gz LAMMPS_GUI
 popd
 
 echo "Cleanup dir"
